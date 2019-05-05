4b38098d08cdd62b3597aac8f445b5c056ad28fb
Lenk <jmvg.comp@gmail.com>
2019-05-04 22:18:29 -0300

    Adição de métodos auxiliares para Department e Seller.

--- src/DAOProject/model/dao/Impl/SellerJDBC.java
+++ src/DAOProject/model/dao/Impl/SellerJDBC.java
@@ -47,16 +47,8 @@ public class SellerJDBC implements DAO<Seller> {
             st.setInt(1, id);
             rs = st.executeQuery();
             if(rs.next()) {
-                Department department = new Department();
-                department.setId(rs.getInt("DepartmentId"));
-                department.setName(rs.getString("DepName"));
-                Seller seller = new Seller();
-                seller.setId(rs.getInt("Id"));
-                seller.setName(rs.getString("Name"));
-                seller.setEmail(rs.getString("Email"));
-                seller.setBaseSalary(rs.getDouble("BaseSalary"));
-                seller.setBirthDate(rs.getDate("BirthDate"));
-                seller.setDepartment(department);
+                Department department = instantiateDepartment(rs);
+                Seller seller = instantiateSeller(rs, department);
                 return seller;
             }
             return null;
@@ -77,5 +69,23 @@ public class SellerJDBC implements DAO<Seller> {
     @Override
     public List<Seller> findAll() {
         return null;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department department = new Department();
+        department.setId(rs.getInt("DepartmentId"));
+        department.setName(rs.getString("DepName"));
+        return department;
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department department) throws SQLException {
+        Seller seller = new Seller();
+        seller.setId(rs.getInt("Id"));
+        seller.setName(rs.getString("Name"));
+        seller.setEmail(rs.getString("Email"));
+        seller.setBaseSalary(rs.getDouble("BaseSalary"));
+        seller.setBirthDate(rs.getDate("BirthDate"));
+        seller.setDepartment(department);
+        return seller;
     }
 }
